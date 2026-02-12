203ac00ab9349fdaa20a595b16391d4a24ad6232
Luthiano Miguel <luthianomiguel@gmail.com>
2026-02-11 22:00:17 -0300

    feat: rotas do crud de atividades

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ActivityController;
 use App\Http\Controllers\Api\SubjectController;
 
 Route::get('/user', function (Request $request) {
@@ -9,3 +10,5 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource("/subjects", SubjectController::class);
+
+Route::apiResource("/activities", ActivityController::class);
