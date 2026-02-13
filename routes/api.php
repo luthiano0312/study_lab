1a6168c11c2135da38b82429d47c23cec851df85
Luthiano Miguel <luthianomiguel@gmail.com>
2026-02-12 21:05:54 -0300

    faet: rotas do crud de notas

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\ActivityController;
+use App\Http\Controllers\Api\GradeController;
+use App\Http\Controllers\Api\SubjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ActivityController;
-use App\Http\Controllers\Api\SubjectController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +13,5 @@ Route::get('/user', function (Request $request) {
 Route::apiResource("/subjects", SubjectController::class);
 
 Route::apiResource("/activities", ActivityController::class);
+
+Route::apiResource("/grades", GradeController::class);
