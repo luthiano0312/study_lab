2ae86335c58a12600bdbdcba8999df111505d466
Luthiano Miguel <luthianomiguel@gmail.com>
2026-02-12 22:11:43 -0300

    feat: rotas do crud de avaliações

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\ActivityController;
 use App\Http\Controllers\Api\GradeController;
 use App\Http\Controllers\Api\SubjectController;
+use App\Http\Controllers\Api\ExamController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,5 @@ Route::apiResource("/subjects", SubjectController::class);
 Route::apiResource("/activities", ActivityController::class);
 
 Route::apiResource("/grades", GradeController::class);
+
+Route::apiResource("/exams", ExamController::class);
