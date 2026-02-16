b31ebcf7efc1bdbee28ea09deb34081aedb38362
natanferreirah <natanferreiradon31@gmail.com>
2026-02-16 20:06:40 -0300

    feat: Adicionar prefixo auth nas rotas de autenticação

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,10 @@ use App\Http\Controllers\Api\SubjectController;
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
