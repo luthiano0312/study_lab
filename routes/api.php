195736b9a35352bf39d6ddc3b3cc3aaf11abbf06
natanferreirah <natanferreiradon31@gmail.com>
2026-02-16 19:55:14 -0300

    feat: Adicionar rotas de Autenticação

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,21 @@
 <?php
 
 use App\Http\Controllers\Api\ActivityController;
+use App\Http\Controllers\Api\ExamController;
 use App\Http\Controllers\Api\GradeController;
 use App\Http\Controllers\Api\SubjectController;
-use App\Http\Controllers\Api\ExamController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::apiResource("/subjects", SubjectController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::apiResource("/activities", ActivityController::class);
-
-Route::apiResource("/grades", GradeController::class);
-
-Route::apiResource("/exams", ExamController::class);
+    Route::apiResource('subjects', SubjectController::class);
+    Route::apiResource('activities', ActivityController::class);
+    Route::apiResource('grades', GradeController::class);
+    Route::apiResource('exams', ExamController::class);
+});
