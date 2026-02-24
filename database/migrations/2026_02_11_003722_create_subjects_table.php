7bc3e2ef437aa819282613438b4b50bebb005d01
Luthiano Miguel <luthianomiguel@gmail.com>
2026-02-24 18:39:06 -0300

    fix: mudança no tipo de dado de semester da tabela subjects

--- database/migrations/2026_02_11_003722_create_subjects_table.php
+++ database/migrations/2026_02_11_003722_create_subjects_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string("name");
             $table->string("abbreviation");
             $table->string("teacher");
-            $table->string("semester");
+            $table->integer("semester");
             $table->timestamps();
         });
     }
